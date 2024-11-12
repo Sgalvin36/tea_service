d1f104e12132e0c30f32371d2154d1970c3149dd
Shane Galvin <Sgalvin36@gmail.com>
2024-11-12 15:23:02 -0700

    Add tests for subscription model method

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe Subscription, type: :model do
         it { should validate_presence_of(:frequency) }
         it { should validate_numericality_of(:price) }
     end
+
+    describe "cancel_subcriptions" do
+        it "updates the status of all customers for a specific subscription" do
+            customer4 = Customer.create!(first_name: "Brandon", last_name: "Bradley", email: "BBLover@email.com", address: "231 Main St.", city: "Denver", state: "CO", zip_code:80502)
+            subscription4 = Subscription.create!(title:"Classic Time", price:4.99, frequency:"Bi-Weekly", status:"active")
+            CustomersSubscription.create!(subscription_id: subscription4.id, customer_id: customer4.id, status: "active")
+        
+            expect(subscription4.customers_subscriptions.first[:status]).to eq("active")
+            subscription4.cancel_subscriptions
+            expect(subscription4.customers_subscriptions.first[:status]).to_not eq("active")
+            expect(subscription4.customers_subscriptions.first[:status]).to eq("canceled")
+        end
+    end
 end
